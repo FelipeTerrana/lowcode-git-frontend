b991c600b6687961e71e5aed06234a82e798629e
Felipe Terrana Cazetta <felipetcazetta@gmail.com>
2023-06-22 07:22:28 -0300

    Enviar conteúdo do script para o backend

--- app/background.js
+++ app/background.js
@@ -1,22 +1,45 @@
 const BACKEND_URL = "http://localhost:8080/commit";
+const GIT_PATH = "dateValidation.js";
 
 chrome.webRequest.onBeforeRequest.addListener(
-  function(details) {
-      const body = decodeBody(details.requestBody.raw[0].bytes);
-      
-      fetch(BACKEND_URL, {
-        "body": JSON.stringify(body),
-        "method": "POST",
-        "mode": "no-cors"
-      });
-  },
-  { urls: ["*://*.budibase.app/*"] },
-  ["requestBody"]
+    function (details) {
+        if (details?.requestBody?.raw[0]?.bytes) {
+            const body = decodeBody(details.requestBody.raw[0].bytes);
+
+            let base64Code = body?.props
+                ?._children[0]
+                ?._children[0]
+                ?._children[0]
+                ?._children[2]
+                ?._children[7]
+                ?.validation[0]?.value;
+
+            if(!base64Code) {
+                return;
+            }
+
+            [, base64Code] = base64Code.match(/^\{\{ js \"(.*)\" \}\}$/);
+
+            fetch(BACKEND_URL, {
+              "body": JSON.stringify({
+                "contentBase64": base64Code,
+                "path": GIT_PATH
+              }),
+              "headers": {
+                "Content-Type": "application/json"
+              },
+              "method": "POST",
+              "mode": "cors"
+            });
+        }
+    },
+    { urls: ["*://*.budibase.app/*"] },
+    ["requestBody"]
 );
 
 
 
 function decodeBody(bytes) {
-  return JSON.parse(decodeURIComponent(String.fromCharCode.apply(null,
-    new Uint8Array(bytes))));
+    return JSON.parse(decodeURIComponent(String.fromCharCode.apply(null,
+        new Uint8Array(bytes))));
 }
