eca5e54b44c2ddfc18fa5f1c12bbe74fd53f45fb
Felipe Terrana Cazetta <felipetcazetta@gmail.com>
2023-07-02 18:41:32 -0300

    Refatorar service worker

--- app/background.js
+++ app/background.js
@@ -1,63 +1,17 @@
-const BACKEND_URL = "http://localhost:8080/commit";
 const GIT_PATH = "dateValidation.js";
 
 chrome.webRequest.onBeforeRequest.addListener(
-    function (details) {
+    details => {
         if (details?.requestBody?.raw[0]?.bytes) {
             const body = decodeBody(details.requestBody.raw[0].bytes);
 
-            let base64Code = body?.props
-                ?._children[0]
-                ?._children[0]
-                ?._children[0]
-                ?._children[2]
-                ?._children[7]
-                ?.validation[0]?.value;
+            let base64Code = getBase64CodeFromRequest(body);
 
             if(!base64Code) {
                 return;
             }
 
-            [, base64Code] = base64Code.match(/^\{\{ js \"(.*)\" \}\}$/);
-
-            chrome.tabs.query({ active: true, currentWindow: true }).then(tabs => {
-                chrome.scripting.executeScript({
-                    "target": { "tabId": tabs[0].id },
-                    "world": "MAIN",
-                    "func": () => {
-                        const userEmail = document.querySelector("#app > div.root.svelte-gzmtw > div.top-nav.svelte-gzmtw > div.toprightnav.svelte-gzmtw > div.avatars.svelte-16y517q > div > div.tooltip.svelte-10v3vdh > span > span.spectrum-Tooltip-label.svelte-12iylyn").innerHTML;
-
-                        return userEmail;
-                    }
-                }).then(results => {
-                    const userEmail = results[0].result;
-
-                    fetch(BACKEND_URL, {
-                      "body": JSON.stringify({
-                          "contentBase64": base64Code,
-                          "path": GIT_PATH,
-                          "authorEmail": userEmail
-                      }),
-                      "headers": {
-                          "Content-Type": "application/json"
-                      },
-                      "method": "POST",
-                      "mode": "cors"
-                    }).then(response => {
-                        response.json().then(body => {
-                            if (body.success) {
-                                console.log("Commit realizado com sucesso");
-                            } else {
-                                console.error("Erro ao realizar commit");
-                                console.error(body)
-                            }
-                        });
-                    }).catch(error => {
-                        console.error("Erro ao fazer requisição");
-                        console.error(error);
-                    });
-                });
-            });
+            commit(GIT_PATH, base64Code);
         }
     },
     { urls: ["*://*.budibase.app/*"] },
@@ -66,7 +20,74 @@ chrome.webRequest.onBeforeRequest.addListener(
 
 
 
+// ----------------------------------------------------
+
+
+
 function decodeBody(bytes) {
     return JSON.parse(decodeURIComponent(String.fromCharCode.apply(null,
         new Uint8Array(bytes))));
+}
+
+
+
+function getBase64CodeFromRequest(body) {
+    let base64Code = body?.props
+        ?._children[0]
+        ?._children[0]
+        ?._children[0]
+        ?._children[2]
+        ?._children[7]
+        ?.validation[0]?.value || "";
+
+    [, base64Code] = base64Code.match(/^\{\{ js \"(.*)\" \}\}$/);
+
+    return base64Code;
+}
+
+
+
+function commit(path, base64Code) {
+    const BACKEND_URL = "http://localhost:8080/commit";
+
+    chrome.tabs.query({ active: true, currentWindow: true })
+        .then(tabs => chrome.scripting.executeScript({
+                "target": { "tabId": tabs[0].id },
+                "world": "MAIN",
+                "func": () => {
+                    const userEmail = document.querySelector("#app > div.root.svelte-gzmtw > div.top-nav.svelte-gzmtw > div.toprightnav.svelte-gzmtw > div.avatars.svelte-16y517q > div > div.tooltip.svelte-10v3vdh > span > span.spectrum-Tooltip-label.svelte-12iylyn").innerHTML;
+
+                    return userEmail;
+                }
+            })
+        )
+        .then(results => {
+            const userEmail = results[0].result;
+
+            return fetch(BACKEND_URL, {
+                "body": JSON.stringify({
+                    "contentBase64": base64Code,
+                    "path": path,
+                    "authorEmail": userEmail
+                }),
+                "headers": {
+                    "Content-Type": "application/json"
+                },
+                "method": "POST",
+                "mode": "cors"
+            });
+        })
+        .then(response => response.json())
+        .then(body => {
+            if (body.success) {
+                console.log("Commit realizado com sucesso");
+            } else {
+                console.error("Erro ao realizar commit");
+                console.error(body)
+            }
+        })
+        .catch(error => {
+            console.error("Erro ao fazer commit");
+            console.error(error);
+        });
 }
